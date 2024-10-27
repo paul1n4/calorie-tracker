7e3e72888b877c2f2dbfa3df5a10ef93ed7a92c3
paul1n4 <paulinevaleroc@gmail.com>
2024-10-27 19:41:22 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { useReducer } from "react"
+import Form from "./components/Form"
+import { activityReducer, initialState } from "./reducers/activity-reducer"
+import ActivityList from "./components/ActivityList";
+
+function App() {
+
+  const[state, dispatch] = useReducer(activityReducer, initialState)
+
+  
+
+  return (
+    <>
+      <header className="bg-lime-600 py-3">
+        <div className="max-w-4xl mx-auto felx justify-between">
+          <h1 className="text-center text-lg font-bold text-white uppercase">
+            Calorie counter
+          </h1>
+
+
+        </div>
+      </header>
+
+      <section className="bg-lime-500 py-20 px-5 ">
+        <div className="max-w-4xl mx-auto">
+          <Form
+            dispatch={dispatch}
+          />
+        </div>
+      </section>
+
+      <section className="p-10 mx-auto max-w-4xl">
+        <ActivityList
+          activities={state.activities}
+          dispatch={dispatch}
+        />
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Form.tsx
@@ -0,0 +1,105 @@
+import { useState, ChangeEvent, FormEvent, Dispatch } from "react"
+import { v4 as uuidv4} from 'uuid'
+import { categories } from "../data/categories"
+import type { Activity } from "../types"
+import { ActivityActions } from "../reducers/activity-reducer"
+
+type FormProps = {
+  dispatch: Dispatch<ActivityActions>
+}
+
+const initialState : Activity ={
+  id: uuidv4(),
+  category: 1,
+    activityName: '',
+    calories: 0
+}
+
+export default function Form({dispatch}: FormProps) {
+
+  const [activity, setActivity] = useState<Activity>(initialState)
+
+  const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
+    const isNumberField = ['category', 'calories'].includes(e.target.id)    
+    setActivity({
+      ...activity,
+      [e.target.id]: isNumberField ? + e.target.value : e.target.value
+    })
+  }
+
+  const isValidActivity = () => {
+    const { activityName, calories} = activity
+    
+    return activityName.trim() !== '' && calories > 0 
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement> ) => {
+    e.preventDefault()
+    
+    dispatch({ type: 'save-activity', payload: {newActivity: activity}})
+
+    setActivity({
+      ...initialState,
+      id: uuidv4()
+    })
+  }
+
+
+
+  return (
+    <form
+      className="space-y-5 bg-white shadow p-10 rounded-lg"
+      onSubmit={handleSubmit}
+    >
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="category" className="font-bold">Category:</label>
+        <select 
+          className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+          id="category"
+          value={activity.category}
+          onChange={handleChange}
+        >
+          {categories.map(category => (
+            <option
+              key={category.id}
+              value={category.id}
+            >
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="activityName" className="font-bold">Activity:</label>
+        <input 
+          id="activityName"
+          type="text" 
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Ex. Food, Orange Juice, Salad, Exercise, Lifting Weights, Cycling"
+          value={activity.activityName}
+          onChange={handleChange}
+        />
+      </div>
+      
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="calories" className="font-bold">Calories:</label>
+        <input 
+          id="calories"
+          type="number" 
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Calories, ex. 300 or 500"
+          value={activity.calories}
+          onChange={handleChange}
+        />
+      </div>
+
+      <input 
+        type="submit" 
+        className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10"
+        value={activity.category === 1 ? 'Save Food' : 'Save Exercise'}
+        disabled={!isValidActivity()}
+      />
+    </form>
+  )
+}
