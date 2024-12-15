10297d7cc5f79945a4bc97653a908762433c373c
paul1n4 <paulinevaleroc@gmail.com>
2024-12-15 14:41:11 -0500

    changes in header div App

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
   return (
     <> 
       <header className="bg-lime-600 py-3">
-        <div className="max-w-4xl mx-auto felx justify-between items-center">
+        <div className="max-w-4xl mx-auto flex justify-between items-center px-3">
           <h1 className="text-center text-lg font-bold text-white uppercase">
             Calorie counter
           </h1>
